664b21926f5a9e860ed82d2bde37b0a77b6cc4bb
SilverShadow123 <hasanmohasin852@gmail.com>
2024-08-28 17:02:35 +0600

    Simple Calculator App

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'function.dart';
 
@@ -18,69 +19,98 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(
+          title: Text('Calculator'),
+          centerTitle: true,
+        ),
         body: Padding(
-      padding: const EdgeInsets.all(10.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          TextField(
-            controller: _controller,
-            focusNode: _focusNode1,
-            keyboardType: TextInputType.none,
+          padding: const EdgeInsets.all(10.0),
+          child: SafeArea(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  TextField(
+                    controller: _controller,
+                    focusNode: _focusNode1,
+                    keyboardType: TextInputType.none,
+                    textAlign: TextAlign.end,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Enter 1st Number',
+                      label: Text('1st Number'),
+                    ),
+                  ),
+                  TextField(
+                    controller: _seccontroller,
+                    focusNode: _focusNode2,
+                    textAlign: TextAlign.end,
+                    keyboardType: TextInputType.none,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Enter 2nd Number',
+                      label: Text('2nd Number'),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 200,
+                  ),
+                  Container(
+                      alignment: Alignment(0, 0),
+                      child: Text(
+                        ' ${_reasult.toStringAsFixed(2)}',
+                        style:
+                            TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                      )),
+                  const SizedBox(
+                    height: 100,
+                  ),
+                  _buttonBar1(),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  _buttonBar2(),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  _buttonBar3(),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  _buttonBar4(),
+                ],
+              ),
+            ),
           ),
-          TextField(
-            controller: _seccontroller,
-            focusNode: _focusNode2,
-            keyboardType: TextInputType.none,
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          Text('Reasult ${_reasult.toStringAsFixed(2)}'),
-
-          _buttonBar1(),
-          const SizedBox(
-            height: 10,
-          ),
-          _buttonBar2(),
-          const SizedBox(
-            height: 10,
-          ),
-          _buttonBar3(),
-
-        ],
-      ),
-    ));
+        ));
   }
 
-  Widget _buttonBar1(){
+  Widget _buttonBar1() {
     return ButtonBar(
-      alignment: MainAxisAlignment.center,
+      alignment: MainAxisAlignment.spaceEvenly,
       children: [
         ElevatedButton(
-          onPressed: () => _appendNumber("1"),
-          child: const Text('1'),
+          onPressed: () => _clear,
+          child: const Text('AC'),
         ),
         ElevatedButton(
-          onPressed: () => _appendNumber("2"),
-          child: const Text('2'),
+          onPressed: () => _appendNumber("8"),
+          child: const Text('8'),
         ),
         ElevatedButton(
-          onPressed: () => _appendNumber("3"),
-          child: const Text('3'),
+          onPressed: () => _appendNumber("7"),
+          child: const Text('7'),
         ),
-        ElevatedButton(
-          onPressed: () => _appendNumber("4"),
-          child: const Text('4'),
-        ),
-        ElevatedButton(
-          onPressed: () => _appendNumber("5"),
-          child: const Text('5'),
-        ),
+        TextButton(
+            onPressed: _addition,
+            child: const Text(
+              '+',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+            )),
       ],
     );
   }
-  Widget _buttonBar2(){
+
+  Widget _buttonBar2() {
     return ButtonBar(
       alignment: MainAxisAlignment.spaceEvenly,
       children: [
@@ -89,13 +119,53 @@ class _HomeScreenState extends State<HomeScreen> {
           child: const Text('6'),
         ),
         ElevatedButton(
-          onPressed: () => _appendNumber("7"),
-          child: const Text('7'),
+          onPressed: () => _appendNumber("5"),
+          child: const Text('5'),
         ),
         ElevatedButton(
-          onPressed: () => _appendNumber("8"),
-          child: const Text('8'),
+          onPressed: () => _appendNumber("4"),
+          child: const Text('4'),
         ),
+        TextButton(
+            onPressed: _subtract,
+            child: const Text(
+              '-',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+            )),
+      ],
+    );
+  }
+
+  Widget _buttonBar3() {
+    return ButtonBar(
+      alignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        ElevatedButton(
+          onPressed: () => _appendNumber("3"),
+          child: const Text('3'),
+        ),
+        ElevatedButton(
+          onPressed: () => _appendNumber("2"),
+          child: const Text('2'),
+        ),
+        ElevatedButton(
+          onPressed: () => _appendNumber("1"),
+          child: const Text('1'),
+        ),
+        TextButton(
+            onPressed: _multiplication,
+            child: const Text(
+              '*',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+            )),
+      ],
+    );
+  }
+
+  Widget _buttonBar4() {
+    return ButtonBar(
+      alignment: MainAxisAlignment.spaceEvenly,
+      children: [
         ElevatedButton(
           onPressed: () => _appendNumber("9"),
           child: const Text('9'),
@@ -104,31 +174,10 @@ class _HomeScreenState extends State<HomeScreen> {
           onPressed: () => _appendNumber("0"),
           child: const Text('0'),
         ),
-      ],
-    );
-  }
-  Widget _buttonBar3(){
-    return ButtonBar(
-      alignment: MainAxisAlignment.center,
-      children: [
-        TextButton(
-            onPressed: _addition,
-            child: const Text(
-              '+',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
-            )),
-        TextButton(
-            onPressed: _subtract,
-            child: const Text(
-              '-',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
-            )),
-        TextButton(
-            onPressed: _multiplication,
-            child: const Text(
-              '*',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
-            )),
+        ElevatedButton(
+          onPressed: () => _appendNumber("."),
+          child: const Text('.'),
+        ),
         TextButton(
             onPressed: _division,
             child: const Text(
@@ -137,6 +186,12 @@ class _HomeScreenState extends State<HomeScreen> {
             )),
       ],
     );
+  }
+
+  void _clear() {
+    _seccontroller.clear();
+    _controller.clear();
+    _reasult = 0;
   }
 
   void _appendNumber(String number) {
