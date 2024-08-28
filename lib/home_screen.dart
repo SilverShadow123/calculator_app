3d73acf50b7b66884f03f0cd8803e7196f7f82c6
SilverShadow123 <hasanmohasin852@gmail.com>
2024-08-28 18:05:23 +0600

    Updating UI

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -27,65 +27,69 @@ class _HomeScreenState extends State<HomeScreen> {
           padding: const EdgeInsets.all(10.0),
           child: SafeArea(
             child: SingleChildScrollView(
+
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-
-                  TextField(
-                    controller: _controller,
-                    focusNode: _focusNode1,
-                    keyboardType: TextInputType.none,
-                    textAlign: TextAlign.end,
-                    decoration: InputDecoration(
-                      border: InputBorder.none,
-                      hintText: 'Enter 1st Number',
-                      label: Text('1st Number'),
-                    ),
+                  Column(
+                    children: [
+                      TextField(
+                        controller: _controller,
+                        focusNode: _focusNode1,
+                        keyboardType: TextInputType.none,
+                        textAlign: TextAlign.end,
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: 'Enter 1st Number',
+                          label: Text('1st Number'),
+                        ),
+                      ),
+                      TextField(
+                        controller: _seccontroller,
+                        focusNode: _focusNode2,
+                        textAlign: TextAlign.end,
+                        keyboardType: TextInputType.none,
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: 'Enter 2nd Number',
+                          label: Text('2nd Number'),
+                        ),
+                      ),
+                    ],
                   ),
-
-                  TextField(
-                    controller: _seccontroller,
-                    focusNode: _focusNode2,
-                    textAlign: TextAlign.end,
-                    keyboardType: TextInputType.none,
-                    decoration: InputDecoration(
-                      border: InputBorder.none,
-                      hintText: 'Enter 2nd Number',
-                      label: Text('2nd Number'),
-                    ),
-                  ),
-
                   const SizedBox(
                     height: 100,
                   ),
-
                   Text(
                     ' ${_reasult.toStringAsFixed(2)}',
                     style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
                   ),
-
                   const SizedBox(
                     height: 100,
                   ),
-
-                  _buttonBar1(),
-
-                  const SizedBox(
-                    height: 10,
+                  const Divider(
+                    color: Colors.grey,
                   ),
-
-                  _buttonBar2(),
-
-                  const SizedBox(
-                    height: 10,
+                  Column(
+                    children: [
+                      const SizedBox(
+                        height: 50,
+                      ),
+                      _buttonBar1(),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      _buttonBar2(),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      _buttonBar3(),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      _buttonBar4(),
+                    ],
                   ),
-
-                  _buttonBar3(),
-
-                  const SizedBox(
-                    height: 10,
-                  ),
-
-                  _buttonBar4(),
                 ],
               ),
             ),
@@ -99,21 +103,36 @@ class _HomeScreenState extends State<HomeScreen> {
       children: [
         ElevatedButton(
           onPressed: _clear,
-          child: const Text('AC'),
+          child: const Text(
+            'AC',
+            style: TextStyle(
+                fontWeight: FontWeight.bold, fontSize: 20, color: Colors.red),
+          ),
         ),
         ElevatedButton(
           onPressed: () => _appendNumber("8"),
-          child: const Text('8'),
+          child: const Text('8',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                  color: Colors.white)),
         ),
         ElevatedButton(
           onPressed: () => _appendNumber("7"),
-          child: const Text('7'),
+          child: const Text('7',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                  color: Colors.white)),
         ),
         TextButton(
             onPressed: _addition,
             child: const Text(
               '+',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24,
+                  color: Colors.greenAccent),
             )),
       ],
     );
@@ -125,21 +144,36 @@ class _HomeScreenState extends State<HomeScreen> {
       children: [
         ElevatedButton(
           onPressed: () => _appendNumber("6"),
-          child: const Text('6'),
+          child: const Text('6',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                  color: Colors.white)),
         ),
         ElevatedButton(
           onPressed: () => _appendNumber("5"),
-          child: const Text('5'),
+          child: const Text('5',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                  color: Colors.white)),
         ),
         ElevatedButton(
           onPressed: () => _appendNumber("4"),
-          child: const Text('4'),
+          child: const Text('4',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                  color: Colors.white)),
         ),
         TextButton(
             onPressed: _subtract,
             child: const Text(
               '-',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24,
+                  color: Colors.greenAccent),
             )),
       ],
     );
@@ -151,21 +185,36 @@ class _HomeScreenState extends State<HomeScreen> {
       children: [
         ElevatedButton(
           onPressed: () => _appendNumber("3"),
-          child: const Text('3'),
+          child: const Text('3',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                  color: Colors.white)),
         ),
         ElevatedButton(
           onPressed: () => _appendNumber("2"),
-          child: const Text('2'),
+          child: const Text('2',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                  color: Colors.white)),
         ),
         ElevatedButton(
           onPressed: () => _appendNumber("1"),
-          child: const Text('1'),
+          child: const Text('1',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                  color: Colors.white)),
         ),
         TextButton(
             onPressed: _multiplication,
             child: const Text(
               '*',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24,
+                  color: Colors.greenAccent),
             )),
       ],
     );
@@ -177,21 +226,36 @@ class _HomeScreenState extends State<HomeScreen> {
       children: [
         ElevatedButton(
           onPressed: () => _appendNumber("9"),
-          child: const Text('9'),
+          child: const Text('9',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                  color: Colors.white)),
         ),
         ElevatedButton(
           onPressed: () => _appendNumber("0"),
-          child: const Text('0'),
+          child: const Text('0',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                  color: Colors.white)),
         ),
         ElevatedButton(
           onPressed: () => _appendNumber("."),
-          child: const Text('.'),
+          child: const Text('.',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                  color: Colors.white)),
         ),
         TextButton(
             onPressed: _division,
             child: const Text(
               '/',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24,
+                  color: Colors.greenAccent),
             )),
       ],
     );
@@ -199,14 +263,10 @@ class _HomeScreenState extends State<HomeScreen> {
 
   void _clear() {
     _seccontroller.clear();
-     _controller.clear();
-     _reasult =0;
-     setState(() {
-
-     });
-
+    _controller.clear();
+    _reasult = 0;
+    setState(() {});
   }
-
 
   void _appendNumber(String number) {
     if (_focusNode1.hasFocus) {
